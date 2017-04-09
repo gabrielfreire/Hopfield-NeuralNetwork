cfadeffbf071d628157945f596e491be1c5bd0c2
Gabriel Freire <Gabriel Freire>
2017-04-09 20:48:25 +0100

    Implemented the HopfieldNetwork class, train and recall methods

--- src/HopfieldNetwork.java
+++ src/HopfieldNetwork.java
@@ -2,4 +2,39 @@
  * Created by Gabriel Freire on 09/04/2017.
  */
 public class HopfieldNetwork {
+
+    private double[][] weightMatrix;
+
+    public HopfieldNetwork(int dimension){
+        this.weightMatrix = new double[dimension][dimension];
+    }
+
+    public void train(double[] pattern){
+        double[] patternBipolar = Utils.transform(pattern);
+        double[][] patternMatrix = Matrix.createMatrix(patternBipolar.length, patternBipolar.length);
+
+        patternMatrix = Matrix.outerProduct(patternBipolar);
+        patternMatrix = Matrix.clearDiagonals(patternMatrix);
+
+        this.weightMatrix = Matrix.addMatrix(this.weightMatrix, patternMatrix);
+    }
+
+    public void recall(double[] pattern){
+        double[] patterBipolar = Utils.transform(pattern);
+        double[] result = Matrix.matrixVectorMultiplication(this.weightMatrix, patterBipolar);
+
+        for(int i = 0; i < patterBipolar.length; i++){
+            result[i] = ActivationFunction.stepFunction(result[i]);
+        }
+
+        for(int i = 0; i < patterBipolar.length; i++){
+            if(patterBipolar[i] != result[i]){
+                System.out.println("Pattern is not recognized...");
+                return;
+            }
+        }
+
+        System.out.println("Pattern is recognized...");
+    }
+
 }
