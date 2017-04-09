275b3f96d66c17437d87b4d500ca6307710722af
Gabriel Freire <Gabriel Freire>
2017-04-09 21:07:48 +0100

    Testing code in Main Class - final commit

--- src/App.java
+++ src/App.java
@@ -2,4 +2,13 @@
  * Created by Gabriel Freire on 09/04/2017.
  */
 public class App {
+    public static void main(String[] args) {
+        HopfieldNetwork hopfieldNetwork = new HopfieldNetwork(4);
+
+        hopfieldNetwork.train(new double[]{1, 0, 1, 0});
+        hopfieldNetwork.train(new double[]{0, 1, 0, 1});
+
+        hopfieldNetwork.recall(new double[]{0, 1, 0, 1});
+        hopfieldNetwork.recall(new double[]{1, 0, 1, 0});
+    }
 }
